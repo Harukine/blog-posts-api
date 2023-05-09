fc150477870725d0477c1150690d4d9728a2b606
Jacob Nguyen <jacobhtnguyen@gmail.com>
2023-05-09 17:09:40 -0400

    applied validation to include user

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,7 +1,14 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, ValidateNested } from 'class-validator';
+import { CreateUserDto } from '../../users/dto/create-user.dto';
+import { Type } from 'class-transformer';
 
 export class CreatePostDto {
   @IsString()
   @IsNotEmpty()
   content: string;
+
+  @ValidateNested()
+  @IsNotEmpty()
+  @Type(() => CreateUserDto)
+  readonly user: CreateUserDto;
 }
