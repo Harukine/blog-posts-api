f440102970924079850453a46294c55968fb7174
Jacob Nguyen <jacobhtnguyen@gmail.com>
2023-05-09 17:03:28 -0400

    applied validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,10 @@
-export class CreateUserDto {}
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsInt()
+  id: number;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly name: string;
+}
