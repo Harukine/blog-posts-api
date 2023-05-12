40f97cfed34e87b5673c812bffb23c2c3a423911
Jacob Nguyen <jacobhtnguyen@gmail.com>
2023-05-12 17:06:42 -0400

    added config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { PostsModule } from './posts/posts.module';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PostsModule, UsersModule, PrismaModule],
+  imports: [ConfigModule.forRoot(), PostsModule, UsersModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
