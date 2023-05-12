8b0fa91504c88fe3f6124465a8e22d970d4b470b
Jacob Nguyen <jacobhtnguyen@gmail.com>
2023-05-12 17:14:46 -0400

    enabled prisma shutdown hooks

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,8 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
   await app.listen(3000);
 }
 
