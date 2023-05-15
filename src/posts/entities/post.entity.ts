5d9b3b569bacf187c555400d1d4c10e841e3b276
Jacob Nguyen <jacobhtnguyen@gmail.com>
2023-05-15 17:56:20 -0400

    implemented post and user entities

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,1 +1,13 @@
-export class Post {}
+import { UserEntity } from '../../users/entities/user.entity';
+
+export class PostEntity {
+  id: number;
+  content: string;
+}
+
+export class PostWithUserEntity {
+  id: number;
+  content: string;
+
+  user: UserEntity;
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,13 @@
-export class User {}
+import { PostEntity } from '../../posts/entities/post.entity';
+
+export class UserEntity {
+  id: number;
+  name: string;
+}
+
+export class UserWithPostsEntity {
+  id: number;
+  name: string;
+
+  posts: PostEntity[];
+}
